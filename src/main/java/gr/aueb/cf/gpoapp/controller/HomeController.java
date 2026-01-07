d34eb2e71fd65bdab75e7490478ec840abf5a59d
Grigoris.K <grigoriskatsanakis@gmail.com>
2026-01-07 11:53:49 +0200

    feat: add HomeController with landing, login, and register mappings

---
+++ src/main/java/gr/aueb/cf/gpoapp/controller/HomeController.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.gpoapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String landing() {
+        return "landing"; // θα ψάξει το templates/landing.html
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+}

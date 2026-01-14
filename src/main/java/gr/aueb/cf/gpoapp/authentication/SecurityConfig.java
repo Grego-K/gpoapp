9accfbcb06f748249d2d30681cb4cd81eb7471f1
Grigoris.K <grigoriskatsanakis@gmail.com>
2026-01-14 12:22:56 +0200

    feat: add security config

---
+++ src/main/java/gr/aueb/cf/gpoapp/authentication/SecurityConfig.java
@@ -0,0 +1,50 @@
+package gr.aueb.cf.gpoapp.authentication;
+
+import gr.aueb.cf.gpoapp.authentication.CustomSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomSuccessHandler customSuccessHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        // Δημόσια URLs και web accessed files
+                        .requestMatchers("/", "/landing", "/login", "/register").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/img/**", "/favicon.ico").permitAll()
+
+                        // Περιορισμοί βάσει authorization role (pharmacist/supplier)
+                        .requestMatchers("/pharmacist/**").hasAuthority("PHARMACIST")
+                        .requestMatchers("/supplier/**").hasAuthority("SUPPLIER")
+
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        // Χρήση του custom success handler για το redirect
+                        .successHandler(customSuccessHandler)
+                        .permitAll()
+                )
+                // Logout
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/") // Redirect στην αρχική
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
+
+        return http.build();
+    }
+}

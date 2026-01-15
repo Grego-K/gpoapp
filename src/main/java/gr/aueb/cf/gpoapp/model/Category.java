567b1f3bb594bd7eb4aaedf16d3b05d8393bb1ec
Grigoris.K <grigoriskatsanakis@gmail.com>
2026-01-15 13:48:40 +0200

    feat: add Category entity

---
+++ src/main/java/gr/aueb/cf/gpoapp/model/Category.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.gpoapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    @Getter(AccessLevel.PRIVATE)
+    private List<Product> products = new ArrayList<>();
+}

ede0157201f208009c0cee6f330c92a73e51065b
Grigoris.K <grigoriskatsanakis@gmail.com>
2026-01-26 00:54:28 +0200

    refactor: implement builder pattern

--- src/main/java/gr/aueb/cf/gpoapp/mapper/UserMapper.java
+++ src/main/java/gr/aueb/cf/gpoapp/mapper/UserMapper.java
@@ -2,28 +2,20 @@ package gr.aueb.cf.gpoapp.mapper;
 
 import gr.aueb.cf.gpoapp.dto.UserInsertDTO;
 import gr.aueb.cf.gpoapp.model.User;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
 
-    /**
-     * Μετατρέπει το DTO σε Entity.
-     * Σημείωση: Το Region δεν τίθεται εδώ, καθώς απαιτεί αναζήτηση στη βάση
-     * μέσω του Service layer.
-     */
-    public User mapToUserEntity(UserInsertDTO userInsertDTO) {
-        User user = new User();
-
-        user.setUsername(userInsertDTO.getUsername());
-        user.setPassword(userInsertDTO.getPassword());
-        user.setFirstname(userInsertDTO.getFirstname());
-        user.setLastname(userInsertDTO.getLastname());
-        user.setEmail(userInsertDTO.getEmail());
-        user.setVat(userInsertDTO.getVat());
-        user.setPhoneNumber(userInsertDTO.getPhonenumber());
-
-        return user;
+    public User mapToUserEntity(UserInsertDTO dto) {
+        return User.builder()
+                .username(dto.getUsername())
+                .password(dto.getPassword())
+                .firstname(dto.getFirstname())
+                .lastname(dto.getLastname())
+                .email(dto.getEmail())
+                .vat(dto.getVat())
+                .phoneNumber(dto.getPhonenumber())
+                .build();
     }
 }

55d38a7d4d47c8cef49200a2e493cf94bd93bd1e
Grigoris.K <grigoriskatsanakis@gmail.com>
2026-01-27 17:03:11 +0200

    feat: add DTO for jwt authentication requests and responses

---
+++ src/main/java/gr/aueb/cf/gpoapp/dto/AuthenticationRequest.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.gpoapp.dto;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}

dd4bed12161f7d3c4a5628ec8c51497d661c2b91
Grigoris.K <grigoriskatsanakis@gmail.com>
2026-01-28 16:26:59 +0200

    feat: add findByUsername to IUserService interface

--- src/main/java/gr/aueb/cf/gpoapp/service/IUserService.java
+++ src/main/java/gr/aueb/cf/gpoapp/service/IUserService.java
@@ -14,4 +14,6 @@ public interface IUserService {
      */
     User registerUser(UserInsertDTO userInsertDTO)
             throws EntityAlreadyExistsException, EntityNotFoundException, EntityInvalidArgumentException;
+
+    User findByUsername(String username);
 }
